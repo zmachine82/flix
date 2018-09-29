aa7c46784f81b0f53fb73d26562808959ec22e84
zmachine82 <zmachine82@gmail.com>
2018-09-29 14:14:38 -0500

    scopes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -17,12 +17,15 @@ class Movie < ApplicationRecord
   RATINGS = %w(G PG PG-13 R NC-17)
   validates :rating, inclusion: { in: RATINGS }
 
+  scope :released, -> { where("released_on <= ?", Time.now).order(released_on: :desc) }
+  scope :hits, -> { released.where('total_gross >= 300000000').order(total_gross: :desc) }
+  scope :flops, -> { released.where('total_gross < 50000000').order(total_gross: :asc) }
+  scope :upcoming, -> {  where("released_on > ?", Time.now).order(released_on: :asc) }
+  scope :rated, ->(rating) { released.where(rating: rating) }
+  scope :recent, ->(max=5) { released.limit(max) }
+
   def flop?
      total_gross.blank? || total_gross < 50000000
-  end
-
-  def self.released
-    where("released_on <= ?", Time.now).order("released_on desc")
   end
 
   def average_stars
